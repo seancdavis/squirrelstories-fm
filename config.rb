b9ba8655eadd5024b81e48a69e1256d0c73521d7
Sean C Davis <scdavis41@gmail.com>
2018-02-23 07:05:37 -0500

    Stuck on imgix -- can't get it working with imgix.js

--- config.rb
+++ config.rb
@@ -5,6 +5,8 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :imgix, host: 'sapwood.imgix.net', secure_url_token: 'qmNcr67W7y89zkKZ', imgix_js_version: 3
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -97,6 +99,30 @@ helpers do
   def quote_path(quote)
     "/quotes/#{quote.episode.id}/#{quote.id}"
   end
+
+  def s3_img_path(s3_url)
+    return '' if s3_url.blank?
+    URI.parse(s3_url).path[1..-1]
+  end
+
+  # def ix_url(image_url, opts = {})
+  #   return '' if image_url.blank?
+  #   options = { auto: 'format' }.merge(opts)
+  #   client = Imgix::Client.new(host: 'sapwood.imgix.net', secure_url_token: 'qmNcr67W7y89zkKZ')
+  #   img_path = URI.parse(image_url).path
+  #   client.path(img_path).to_url(options)
+  # end
+
+  # def ix_img(orig_url, ix_opts = {}, img_opts = {})
+  #   return '' if orig_url.blank?
+  #   ix_opts = { auto: 'format' }.merge(ix_opts || {})
+  #   img_url = ix_url(orig_url, ix_opts)
+  #   attrs = ''
+  #   attrs += "class=\"#{img_opts[:class]}\" " if img_opts[:class]
+  #   # raise img_url
+
+  #   "<img ix-src=\"#{img_url}\" #{attrs}>"
+  # end
 
 end
 
